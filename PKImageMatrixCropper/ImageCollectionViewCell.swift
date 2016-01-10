ccf151aeaa0dd1840b4439c47b1126d6db5f2511
Pavan Kataria <kataria.pavan@gmail.com>
2016-01-10 04:01:53 +0000

    Initial commit - and complete v1

---
+++ PKImageMatrixCropper/ImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionViewCell.swift
+//  PKImageMatrixCropper
+//
+//  Created by Pavan Kataria on 10/01/2016.
+//  Copyright © 2016 Pavan Kataria. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var croppedImageView: UIImageView!
+    func setImage(image: UIImage){
+        print("image ", image)
+        print("yep")
+        croppedImageView.image = image
+        print("yep")
+    }
+}
